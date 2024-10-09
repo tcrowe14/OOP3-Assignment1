ae753041c476b055e84e57c1aa510ff59c664627
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-09 10:42:40 -0600

    Update Cone.java
    added cone stuff

--- src/shapes/Cone.java
+++ src/shapes/Cone.java
@@ -1,6 +1,40 @@
 package shapes;
 
-public class Cone
+public class Cone extends Shape
 {
+	private double radius;
+	
+	public Cone()
+	{
+		
+	}
+	
+	public Cone(double height, double radius)
+	{
+		super(height);
+		this.setRadius(radius);
+	}
+	 
+	public double getRadius()
+	{
+		return radius;
+	}
+
+	public void setRadius(double radius)
+	{
+		this.radius = radius;
+	}
+
+	@Override
+	public double getVolume()
+	{
+		return (1.0 / 3.0) * Math.PI * Math.pow(radius, 2) * this.height;
+	}
+
+	@Override
+	public double getBaseArea()
+	{
+		return Math.PI * Math.pow(radius, 2);
+	}
 
 }
