5aa27614e532a80e91f4decc906eeb6e8133a36f
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-09 12:33:00 -0600

    Update Pyramid.java

--- src/shapes/Pyramid.java
+++ src/shapes/Pyramid.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class Pyramid
+public class Pyramid extends Shape
 {
+	private double sideLength;
 
+    public Pyramid() 
+    {
+        
+    }
+
+    public Pyramid(double height, double sideLength) 
+    {
+        super(height);
+        this.sideLength = sideLength;
+    }
+
+    public double getBaseSideLength() 
+    {
+        return sideLength;
+    }
+
+    public void setBaseSideLength(double sideLength) 
+    {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getVolume() 
+    {
+        return (1.0 / 3.0) * getBaseArea() * getHeight();
+    }
+
+    @Override
+    public double getBaseArea() 
+    {
+        return Math.pow(sideLength, 2);
+    }
 }
