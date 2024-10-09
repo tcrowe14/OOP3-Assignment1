b1ad5e284d9bc3fb11786e584c5d5dfe2d364418
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-09 12:29:23 -0600

    Update Cylinder.java

--- src/shapes/Cylinder.java
+++ src/shapes/Cylinder.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class Cylinder
+public class Cylinder  extends Shape
 {
+	private double radius;
 
+    public Cylinder() 
+    {
+
+    }
+
+    public Cylinder(double height, double radius) 
+    {
+        super(height);
+        this.radius = radius;
+    }
+
+    public double getRadius() 
+    {
+        return radius;
+    }
+
+    public void setRadius(double radius) 
+    {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getVolume() 
+    {
+        return Math.PI * Math.pow(radius, 2) * getHeight();
+    }
+
+    @Override
+    public double getBaseArea() 
+    {
+        return Math.PI * Math.pow(radius, 2);
+    }
 }
