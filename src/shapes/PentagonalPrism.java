5f6d4ec2a3a20ad99e9d027b1a478913263b955a
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-10 07:37:40 -0600

    Update PentagonalPrism.java

--- src/shapes/PentagonalPrism.java
+++ src/shapes/PentagonalPrism.java
@@ -1,6 +1,40 @@
 package shapes;
 
-public class PentagonalPrism
+public class PentagonalPrism extends Shape
 {
+	private double sideLength;
 
+    public PentagonalPrism() 
+    {
+        
+    }
+
+    public PentagonalPrism(double height, double sideLength) 
+    {
+        super(height);
+        this.sideLength = sideLength;
+    }
+
+    public double getSideLength() 
+    {
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength) 
+    {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getVolume() 
+    {
+        return getBaseArea() * getHeight();
+    }
+
+    @Override
+    public double getBaseArea() 
+    {
+        // The base area of a regular pentagon
+        return (5.0 / 4.0) * Math.tan(Math.PI / 5) * Math.pow(sideLength, 2);
+    }
 }
