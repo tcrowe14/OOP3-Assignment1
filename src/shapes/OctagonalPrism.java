459dc470144f1e9d188baf0f2b9a26fa82c32177
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-10 07:36:43 -0600

    Update OctagonalPrism.java

--- src/shapes/OctagonalPrism.java
+++ src/shapes/OctagonalPrism.java
@@ -1,6 +1,41 @@
 package shapes;
 
-public class OctagonalPrism
+public class OctagonalPrism extends Shape
 {
+	private double sideLength;
+	
+	public OctagonalPrism()
+	{
+		
+	}
+	
+	public OctagonalPrism(double height, double sideLength)
+	{
+		super(height);
+		this.setsideLength(sideLength);
+	}
+	
+	
+	public double getsideLength()
+	{
+		return sideLength;
+	}
+
+	public void setsideLength(double sideLength)
+	{
+		this.sideLength = sideLength;
+	}
+
+	@Override
+	public double getVolume()
+	{
+		return getBaseArea() * getHeight();
+	}
+
+	@Override
+	public double getBaseArea()
+	{
+		return 2 * Math.pow(sideLength, 2) * Math.tan(Math.PI / 8);
+	}
 
 }
