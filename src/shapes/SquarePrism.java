83869890de9ee52f5843187d0151f09457223f23
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-10 07:38:07 -0600

    Update SquarePrism.java

--- src/shapes/SquarePrism.java
+++ src/shapes/SquarePrism.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class SquarePrism
+public class SquarePrism extends Shape
 {
+	private double sideLength;
 
+    public SquarePrism() 
+    {
+        
+    }
+
+    public SquarePrism(double height, double sideLength) 
+    {
+        super(height);
+        this.sideLength = sideLength;
+    }
+
+    public double getsideLength() 
+    {
+        return sideLength;
+    }
+
+    public void setsideLength(double sideLength) 
+    {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getVolume() 
+    {
+        return getBaseArea() * getHeight();
+    }
+
+    @Override
+    public double getBaseArea() 
+    {
+        return Math.pow(sideLength, 2);
+    }
 }
