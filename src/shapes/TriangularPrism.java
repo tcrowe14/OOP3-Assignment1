579887c27e91b9cfcabb02caa9b684676f700ea1
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2024-10-10 07:38:33 -0600

    Update TriangularPrism.java

--- src/shapes/TriangularPrism.java
+++ src/shapes/TriangularPrism.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class TriangularPrism
+public class TriangularPrism extends Shape
 {
+	private double sideLength;
 
+    public TriangularPrism() 
+    {
+    	
+    }
+
+    public TriangularPrism(double height, double sideLength) 
+    {
+        super(height);
+        this.sideLength = sideLength;
+    }
+
+    public double getsideLength() 
+    {
+        return sideLength;
+    }
+
+    public void setsideLength(double sideLength) 
+    {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getVolume() 
+    {
+        return (1.0 / 2.0) * getBaseArea() * getHeight();
+    }
+
+    @Override
+    public double getBaseArea() 
+    {
+        return (Math.sqrt(3) / 4.0) * Math.pow(sideLength, 2);
+    }
 }
