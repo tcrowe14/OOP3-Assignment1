7acaad117028a5f6bdac6f88d19ae3459309c91b
Dally-Bear <152315550+Dally-Bear@users.noreply.github.com>
2024-10-15 19:28:31 -0600

    Update InsertionSort.java

--- src/utilities/InsertionSort.java
+++ src/utilities/InsertionSort.java
@@ -1,23 +1,42 @@
+
+
 package utilities;
 
-public class InsertionSort {
-	
-	public static void insertionSort( Integer[] nums) {
-		
-		for (int i = 1; i < nums.length; i++) {
-			int currentValue = nums [i];
-			int j = i - 1;
-			
-			while(j >= 0 && nums[j] > currentValue) {
-				nums[j+1] = nums[j];
-				j--;
-			}
-			
-		nums[j+1] = currentValue;
-		
-		}	
-		
-	}
+import java.util.Comparator;
+import shapes.Shape;
 
+public class InsertionSort {
+
+    
+    public static void insertionSort(Shape[] shapes) {
+        for (int i = 1; i < shapes.length; i++) {
+            Shape currentValue = shapes[i];
+            int j = i - 1;
+
+            // Change this to < 0 if you want descending order
+            while (j >= 0 && shapes[j].compareTo(currentValue) < 0) { 
+                shapes[j + 1] = shapes[j];
+                j--;
+            }
+
+            shapes[j + 1] = currentValue;
+        }
+    }
+
+    // Sort using a custom Comparator
+    public static void insertionSort(Shape[] shapes, Comparator<? super Shape> cmp) {
+        for (int i = 1; i < shapes.length; i++) {
+            Shape currentValue = shapes[i];
+            int j = i - 1;
+
+            // Change this to < 0 if you want descending order
+            while (j >= 0 && cmp.compare(shapes[j], currentValue) < 0) {
+                shapes[j + 1] = shapes[j];
+                j--;
+            }
+
+            shapes[j + 1] = currentValue;
+        }
+    }
 }
 
