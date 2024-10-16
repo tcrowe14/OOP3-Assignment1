3ee437756f119593f33fa0a8ba3ec2a13f229853
Dally-Bear <152315550+Dally-Bear@users.noreply.github.com>
2024-10-15 19:15:54 -0600

    Update BubbleSort.java

--- src/utilities/BubbleSort.java
+++ src/utilities/BubbleSort.java
@@ -12,7 +12,7 @@ public class BubbleSort {
             swapped = false;
             for (int i = 0; i < shapes.length - 1; i++) {
                 // Compare adjacent elements
-                if (shapes[i].compareTo(shapes[i + 1]) > 0) {
+                if (shapes[i].compareTo(shapes[i + 1]) < 0) {
                     swapped = true;
                     // Swap shapes[i] and shapes[i + 1]
                     Shape temp = shapes[i];
