4765930a2d38827d11a129acf520149a319d4152
Dally-Bear <152315550+Dally-Bear@users.noreply.github.com>
2024-10-17 13:45:37 -0600

    Update BubbleSort.java

--- src/utilities/BubbleSort.java
+++ src/utilities/BubbleSort.java
@@ -3,26 +3,51 @@ package utilities;
 import java.util.Comparator;
 import shapes.Shape;
 
+/**
+ * The BubbleSort class provides static methods to sort a Shape object array
+ * using the bubble sort algorithm. It supports sorting arrays either using the
+ * natural ordering of the elements, if they implement Comparable, or using a
+ * specified Comparator.
+ */
 public class BubbleSort {
-
-    public static void bubbleSort(Shape[] shapes) {
+	
+	/**
+	 * Sorts the specified array of Shape objects in descending order, according
+	 * to the natural ordering of its elements. All elements in the array must implement
+	 * the Comparable interface.
+	 *
+	 * @param <Shape> the type of elements in the array, which must extend {@code Comparable}
+	 * @param shapes the array to be sorted
+	 * @throws ClassCastException if the array contains elements that are not mutually comparable
+	 */
+    public static void bubbleSort(Comparable<Shape> shapes[]) {
         boolean swapped = true;
 
         while (swapped) {
             swapped = false;
             for (int i = 0; i < shapes.length - 1; i++) {
                 // Compare adjacent elements
-                if (shapes[i].compareTo(shapes[i + 1]) < 0) {
+                if (shapes[i].compareTo((Shape) shapes[i + 1]) < 0) {
                     swapped = true;
                     // Swap shapes[i] and shapes[i + 1]
-                    Shape temp = shapes[i];
+                    Shape temp = (Shape) shapes[i];
                     shapes[i] = shapes[i + 1];
                     shapes[i + 1] = temp;
                 }
             }
         }
     }
-
+    
+    /**
+     * Sorts the specified array of Shape objects in descending order, according
+     * to the order induced by the specified Comparator. All elements in the array
+     * must be mutually comparable by the specified Comparator.
+     *
+     * @param shapes the array to be sorted
+     * @param cmp the comparator to determine the order of the array
+     * @throws ClassCastException if the array contains elements that are not mutually comparable
+     *         using the specified comparator
+     */
     public static void bubbleSort(Shape[] shapes, Comparator<? super Shape> cmp) {
         boolean swapped = true;
 
@@ -41,4 +66,3 @@ public class BubbleSort {
         }
     }
 }
-
